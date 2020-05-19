135ba8776c7d51099c0a9f7d3a9753a5c0c5fc39
jesubellido <jesubellido@gmail.com>
2020-05-19 08:56:23 -0500

    CRUD Users

--- web/server.py
+++ web/server.py
@@ -31,6 +31,68 @@ def authenticate():
     json_msg = json.dumps(r_msg)
     return Response(json_msg, status=401)
 
+#CRUD users
+@app.route('/users', methods = ['POST'])
+def create_user():
+    #c = json.loads(request.data)
+    c = json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    r_msg = {'msg':'UserCreated'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=201)
+
+
+@app.route('/users/<id>', methods = ['GET'])
+def get_user(id):
+    db_session = db.getSession(engine)
+    users = db_session.query(entities.User).filter(entities.User.id == id)
+    for user in users:
+        js = json.dumps(user, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(json.dumps(message), status=404, mimetype='application/json')
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.User)
+    data = dbResponse[:]
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c = json.loads(request.form['values'])
+
+    for key in c.keys():
+        setattr(user, key, c[key])
+
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+@app.route('/users', methods = ['DELETE'])
+def delete_user():
+    id = request.form['key']
+    session = db.getSession(engine)
+    user = session.query(entities.User).filter(entities.User.id == id).one()
+    session.delete(user)
+    session.commit()
+    return "Deleted User"
+
+
+
+
 if __name__ == '__main__':
     app.secret_key = ".."
     app.run(port=8080, threaded=True, host=('127.0.0.1'))
